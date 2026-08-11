c1bb5b1ab2271a6bb9109e1430b4a64f610ded20
Austin-Labs13 <austingafa@hotmail.com>
2026-08-11 19:36:31 +1000

    Add files via upload

---
+++ 05-PowerShell-Automation/Disable-AuroraEmployee.ps1
@@ -0,0 +1,37 @@
+﻿# Aurora CloudSec Offboarding Script
+
+$Username = (Read-Host "Enter username of employee to offboard").Trim()
+$User = Get-ADUser -Filter "SamAccountName -eq '$Username'"
+if ($null -eq $User) {
+Write-Host "User not found. Please check the username and try again."
+exit
+}
+
+Write-Host "Employee found:"
+Write-Host "Name: $($User.Name)"
+Write-Host "Username: $($User.SamAccountName)"
+
+$Confirm = Read-Host "Type YES to confirm offboarding"
+if ($Confirm.ToUpper() -ne "YES") {
+Write-Host "Offboarding Cancelled."
+exit
+}
+
+Disable-ADAccount -Identity $User
+Write-Host "Account disabled succesfully."
+
+$Groups = Get-ADPrincipalGroupMembership -Identity $User | Where-Object { $_.Name -ne "Domain Users" }
+foreach ($Group in $Groups) {
+Remove-ADGroupMember -Identity $Group.DistinguishedName -Members $Username
+}
+
+Move-ADObject -Identity $User.DistinguishedName -TargetPath "OU=Disabled Users,DC=aurora,DC=local"
+Write-Host "User moved to Disabled Users."
+
+Write-Host "Offboarding complete. Final account state:"
+
+Get-ADUser -Identity $Username |
+Select-Object Name, SamAccountName, Enabled, DistinguishedName
+
+Get-ADPrincipalGroupMembership -Identity $Username |
+Select-Object Name
---
+++ 05-PowerShell-Automation/New-AuroraEmployee.ps1
@@ -0,0 +1,74 @@
+﻿# Aurora CloudSec - New Employee Onboarding Script
+do {
+$FirstName = (Read-Host "Enter first name").Trim()
+if ([string]::IsNullOrWhiteSpace($FirstName)) {
+Write-Host "No first name entered. Please try again."
+}
+} until (-not [string]::IsNullOrWhiteSpace($FirstName))
+do {
+$LastName = (Read-Host "Enter last name").Trim()
+if ([string]::IsNullOrWhiteSpace($LastName)) {
+Write-Host "No last name entered. Please try again."
+}
+} until (-not [string]::IsNullOrWhiteSpace($LastName))
+
+do {
+$Department = (Read-Host "Enter department").Trim().ToUpper()
+if ($Department -notin @("HR", "Finance", "Sales", "IT")) {
+Write-Host "Invalid department. Please enter HR, Finance, Sales, or IT."
+}
+} until ($Department -in @("HR", "Finance", "Sales", "IT"))
+$Username = ($FirstName.Substring(0,1) + $LastName).ToLower()
+$UPN = "$Username@aurora.local"
+$FullName = "$FirstName $LastName"
+if (Get-ADUser -Filter "SamAccountName -eq '$Username'") {
+Write-Host "A user with the username $Username already exists."
+exit
+}
+# Select department OU and security group
+
+switch ($Department) {
+"HR" {
+$OU = "OU=Users,OU=HR,DC=aurora,DC=local"
+$Group = "HR Staff"
+}
+"Finance" {
+$OU = "OU=Users,OU=Finance,DC=aurora,DC=local"
+$Group = "Finance Staff"
+}
+"Sales" {
+$OU = "OU=Users,OU=Sales,DC=aurora,DC=local"
+$Group = "Sales Staff"
+}
+"IT" {
+$OU = "OU=Users,OU=IT,DC=aurora,DC=local"
+$Group = "IT Support"
+}
+default {
+Write-Host "Invalid department. Please use HR, Finance, Sales, or IT."
+exit
+}
+}
+
+#Ask for temporary password
+
+$Password = Read-Host "Enter temporary password" -AsSecureString
+
+
+# Create the Active Directory user
+New-ADUser `
+-Name $FullName `
+-GivenName $FirstName `
+-Surname $LastName `
+-SamAccountName $Username `
+-UserPrincipalName $UPN `
+-Path $OU `
+-AccountPassword $Password `
+-Enabled $true
+
+# Add user to department security group
+Add-ADGroupMember -Identity $Group -Members $Username
+
+#Verify account creation
+Get-ADUser -Identity $Username | Select-Object Name, SamAccountName, Enabled
+Get-ADPrincipalGroupMembership -Identity $Username | Select-Object Name
